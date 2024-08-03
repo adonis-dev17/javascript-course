ae3c3fa9a8dfefb60b6d50e524447b7bf5defbb6
Adonis Medrocillo <adonis.dev17@gmail.com>
2024-08-03 22:42:26 +0800

    create only one button is on

--- Exercises/10f-toggle-buttons.js
+++ Exercises/10f-toggle-buttons.js
@@ -2,8 +2,18 @@ function toggleButton(selector) {
   const buttonElem = document.querySelector(selector);
 
   if (!buttonElem.classList.contains("is-toggled")) {
+    /*before turning on, check if any previous button is on and turn it off */
+    turnOffPrevious();
     buttonElem.classList.add("is-toggled");
   } else {
     buttonElem.classList.remove("is-toggled");
+  }
+}
+
+function turnOffPrevious() {
+  const checker = document.querySelector(".is-toggled");
+
+  if (checker) {
+    checker.classList.remove("is-toggled");
   }
 }
