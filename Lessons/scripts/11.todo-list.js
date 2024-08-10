ad03c36b4568da9bc2bd61c57986aa363076056e
Adonis Medrocillo <adonis.dev17@gmail.com>
2024-08-10 11:14:08 +0800

    add render todoList using generating HTML technique

--- Lessons/scripts/11.todo-list.js
+++ Lessons/scripts/11.todo-list.js
@@ -1,4 +1,19 @@
-const todoList = [];
+const todoList = ["make lunch", "study Javascript", "practice interview"];
+
+renderTodoList();
+
+function renderTodoList() {
+  let todoListHTML = "";
+
+  for (let i = 0; i < todoList.length; i++) {
+    const todo = todoList[i];
+    const html = `<p>${todo}</p>`;
+    todoListHTML += html;
+  }
+  console.log(todoListHTML);
+
+  document.querySelector(".js-todo-list").innerHTML = todoListHTML;
+}
 
 function addTodo() {
   const inputElement = document.querySelector(".js-name-input");
@@ -9,4 +24,6 @@ function addTodo() {
   console.log(todoList);
 
   inputElement.value = "";
+
+  renderTodoList();
 }
