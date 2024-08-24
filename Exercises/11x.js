40979d523262583e56abd7c4a3406ff3b1668fea
Adonis Medrocillo <adonis.dev17@gmail.com>
2024-08-24 23:27:40 +0800

    add save to localStorage

---
+++ Exercises/11x.js
@@ -0,0 +1,71 @@
+// When loading the page, load from localStorage.
+const todoList = JSON.parse(localStorage.getItem("todoList")) || [
+  {
+    name: "make dinner",
+    dueDate: "2024-08-13",
+  },
+  {
+    name: "wash dishes",
+    dueDate: "2024-08-13",
+  },
+];
+
+renderTodoList();
+
+function renderTodoList() {
+  let todoListHTML = "";
+
+  for (let i = 0; i < todoList.length; i++) {
+    const todoObject = todoList[i];
+
+    // const name = todoObject.name;
+    // const dueDate = todoObject.dueDate;
+
+    //change those above using Destructuring technique
+
+    const { name, dueDate } = todoObject;
+
+    // this is generating html technique
+    const html = `
+    <div>${name}</div>
+    <div>${dueDate}</div>  
+    <button onclick="
+      todoList.splice(${i}, 1);
+      renderTodoList();
+    " class="delete-todo-button">Delete</button>
+    `;
+    todoListHTML += html;
+  }
+
+  document.querySelector(".js-todo-list").innerHTML = todoListHTML;
+}
+
+function addTodo() {
+  const inputElement = document.querySelector(".js-name-input");
+
+  const name = inputElement.value;
+
+  const dateInputElement = document.querySelector(".js-due-date-input");
+
+  const dueDate = dateInputElement.value;
+
+  todoList.push({
+    // name: name,
+    // dueDate: dueDate,
+    //make those above using a shorthand property
+
+    name,
+    dueDate,
+  });
+
+  inputElement.value = "";
+
+  renderTodoList();
+
+  // Whenever we update the todo list, save in localStorage.
+  saveToStorage();
+}
+
+function saveToStorage() {
+  localStorage.setItem("todoList", JSON.stringify(todoList));
+}
